90c88148f050a973d69496e5629a0bad60d09ae0
Margarita Khodzka <mkhodzka@gmail.com>
2017-08-01 17:54:32 +0000

    added courses, new client, show client views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -19,14 +19,5 @@ class UserController < ApplicationController
   end
 
 
-  helpers do
-    def logged_in?
-      !!session[:user_id]
-    end
-
-    def current_user
-      User.find_by_id(session[:user_id])
-    end
-  end
 
 end
